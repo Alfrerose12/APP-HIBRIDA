90ce5c9aab8b505c36d026cfc7457e6df2487733
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-10 08:13:13 -0600

    Modificación del login de la aplicación

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router'; // Importa Router
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false
+  standalone: false,
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  @ViewChild('loginForm') loginForm!: NgForm; 
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/tabs/inicio']);
+      this.loginForm.reset();
+    } else {
+
+    }
   }
 
+  // Usar este código si se va a hacer una petición HTTP
+  // onSubmit() {
+  //   if (this.loginForm.valid) {
+  //     const loginData = { email: this.email, password: this.password };
+
+  //     this.http.post('https://tudominio.com/api/login', loginData).subscribe({
+  //       next: (response) => {
+  //         this.router.navigate(['/tabs/inicio']);
+  //         this.loginForm.reset();
+  //       },
+  //       error: (err) => {
+  //       }
+  //     });
+  //   }
+  // }
 }
