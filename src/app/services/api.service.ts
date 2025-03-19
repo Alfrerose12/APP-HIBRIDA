b1107bf0eafa0e87064a8cf08ccca940c701281b
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-19 12:35:31 -0600

    Feat: Implementación para utilizar API

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl = 'http://localhost:3004/api'
+
+  constructor(private http : HttpClient) { }
+
+  login(credentials: {email: string, password: string}): Observable<any> {
+    const endpoint = 'login';
+    return this.http.post(`${this.apiUrl}/${endpoint}`, credentials);
+  }
+
+  register(credentials: {name: string, email: string, password: string}): Observable<any> {
+    const endpoint = 'register';
+    return this.http.post(`${this.apiUrl}/${endpoint}`, credentials);
+  }
+}
